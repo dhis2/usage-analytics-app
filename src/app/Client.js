6ac9af6d507188fb244475c64a729244fb2c5630
Yrjan A. F. Fraschetti <yrjanaff@1x-193-157-248-3.uio.no>
2016-03-30 11:58:08 +0200

    Tried new tables

--- src/app/Client.js
+++ src/app/Client.js
@@ -44,3 +44,32 @@ export default React.createClass({
         else return null;
     }
 });
+
+/*
+ <Table
+ rowHeight={50}
+ rowsCount={rows.length}
+ width={5000}
+ height={5000}
+ headerHeight={50}>
+ <Column
+ header={<Cell>Col 1</Cell>}
+ cell={<Cell>Column 1 static content</Cell>}
+ width={2000}
+ />
+ <Column
+ header={<Cell>Col 2</Cell>}
+ cell={<MyCustomCell mySpecialProp="column2" />}
+ width={1000}
+ />
+ <Column
+ header={<Cell>Col 3</Cell>}
+ cell={({rowIndex, ...props}) => (
+ <Cell {...props}>
+ Data for column 3: {rows[rowIndex][2]}
+ </Cell>
+ )}
+ width={2000}
+ />
+ </Table>
+ */
--- src/app/Table.js
+++ src/app/Table.js
@@ -15,6 +15,7 @@ import Colors from 'material-ui/lib/styles/colors';
 import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
 import TextField from 'material-ui/lib/text-field';
 import Toggle from 'material-ui/lib/toggle';
+import {Table, Column, Cell} from 'fixed-data-table';
 
 const styles = {
     spacing: Spacing,
@@ -34,124 +35,28 @@ const styles = {
     }
 };
 
-export default class Table extends React.Component{
+/*const TextCell = ({rowIndex, data, col, ...props}) => (
+    data.map((result) =>
+        <Cell {...props}>
+            {result.year}
+        </Cell>
+    )
+);*/
 
-    constructor(props) {
-        super(props);
+export default React.createClass({
 
-        this.state = {
-            fixedHeader: false,
-            fixedFooter: true,
-            stripedRows: true,
-            showRowHover: false,
-            selectable: false,
-            multiSelectable: false,
-            enableSelectAll: false,
-            deselectOnClickaway: true,
-            height: '300px',
-            displaySelectAll: false,
-            adjustForCheckbox: false,
-            displayRowCheckbox: false,
-            bodyStyle: { overflowX: undefined, overflowY: undefined }
-        };
-    }
-
-    handleToggle = (event, toggled) => {
-        this.setState({
-            [event.target.name]: toggled,
-        });
-    };
-
-    handleChange = (event) => {
-        this.setState({height: event.target.value});
-    };
-
-
-    render() {
+    render: function() {
 
         return (
-            <div id="scroller">
-            <MaterialTable
-                //bodyStyle={this.state.bodyStyle}
-                //style={{display: 'inline'}}
-                height={this.state.height}
-                fixedHeader={this.state.fixedHeader}
-                fixedFooter={this.state.fixedFooter}
-                selectable={this.state.selectable}
-                multiSelectable={this.state.multiSelectable}
-                onRowSelection={this._onRowSelection}
+            <Table
+                rowHeight={50}
+                rowsCount={this.props.data.length}
+                width={5000}
+                height={5000}
+                headerHeight={50}
             >
-                <TableHeader
-                    enableSelectAll={this.state.enableSelectAll}
-                    displaySelectAll={this.state.displaySelectAll}
-                    adjustForCheckbox={this.state.adjustForCheckbox}>
-                    <TableRow>
-                        <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{textAlign: 'center'}}>
-                            Super Header
-                        </TableHeaderColumn>
-                    </TableRow>
-                    <TableRow>
-                        <TableHeaderColumn tooltip="The Year">Year</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Active Users">Active Users</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Map Views">Map Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Chart Views">Chart Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Report Tables Views">Report Tables Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Event Report Views">Event Report Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Event Chart Views">Event Chart Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Dashboard Views">Dashboard Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Indicators Views">Indicators Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Total Views">Total Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Average Views">Average Views</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Saved Maps">Saved Maps</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Saved Charts">Saved Charts</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Saved Report Tables">Saved Report Tables</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Saved Event Reports">Saved Event Reports</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Saved Event Charts">Saved Event Charts</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Saved Dashboards">Saved Dashboards</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Saved Indicators">Saved Indicators</TableHeaderColumn>
-                        <TableHeaderColumn tooltip="Users">Users</TableHeaderColumn>
-                    </TableRow>
-                </TableHeader>
 
-                <TableBody
-                    deselectOnClickaway={this.state.deselectOnClickaway}
-                    showRowHover={this.state.showRowHover}
-                    stripedRows={this.state.stripedRows}
-                    displayRowCheckbox={this.state.displayRowCheckbox}
-                >
-                    {this.props.data.map( (result) => (
-                        <TableRow key={result.year}>
-                            <TableRowColumn>{result.year}</TableRowColumn>
-                            <TableRowColumn>{result.activeUsers}</TableRowColumn>
-                            <TableRowColumn>{result.mapViews}</TableRowColumn>
-                            <TableRowColumn>{result.chartViews}</TableRowColumn>
-                            <TableRowColumn>{result.reportTablesViews}</TableRowColumn>
-                            <TableRowColumn>{result.eventReportViews}</TableRowColumn>
-                            <TableRowColumn>{result.eventChartViews}</TableRowColumn>
-                            <TableRowColumn>{result.dashboardViews}</TableRowColumn>
-                            <TableRowColumn>{result.indicatorsViews}</TableRowColumn>
-                            <TableRowColumn>{result.totalViews}</TableRowColumn>
-                            <TableRowColumn>{result.averageViews}</TableRowColumn>
-                            <TableRowColumn>{result.savedMaps}</TableRowColumn>
-                            <TableRowColumn>{result.savedCharts}</TableRowColumn>
-                            <TableRowColumn>{result.savedReportTables}</TableRowColumn>
-                            <TableRowColumn>{result.savedEventReports}</TableRowColumn>
-                            <TableRowColumn>{result.savedEventCharts}</TableRowColumn>
-                            <TableRowColumn>{result.savedDashboards}</TableRowColumn>
-                            <TableRowColumn>{result.savedIndicators}</TableRowColumn>
-                            <TableRowColumn>{result.users}</TableRowColumn>
-                        </TableRow>
-                    ))}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TableRowColumn colSpan="3" style={{textAlign: 'center'}}>
-                            Super Footer
-                        </TableRowColumn>
-                    </TableRow>
-                </TableFooter>
-            </MaterialTable>
-                    </div>
+            </Table>
         );
     }
-};
+});
