4307d8229799bffde684b72c387340ab3f2f2fd4
Julie Hill Roa <julie.hill.roa@gmail.com>
2016-03-31 16:46:22 +0200

    changes colortheme and css

--- src/app/App.js
+++ src/app/App.js
@@ -6,11 +6,13 @@ import DropDownMenu from 'material-ui/lib/DropDownMenu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import Checkbox from 'material-ui/lib/checkbox';
 import ActionFavoriteBorder from 'material-ui/lib/svg-icons/action/favorite-border';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
 import HeaderBar from 'd2-ui/lib/header-bar/HeaderBar.component';
 import Client from './Client.js';
+import MyRawTheme from '../colortheme';
 
 
 export default React.createClass({
@@ -21,11 +23,13 @@ export default React.createClass({
 
     childContextTypes: {
         d2: React.PropTypes.object,
+        muiTheme: React.PropTypes.object
     },
 
     getChildContext() {
         return {
             d2: this.props.d2,
+            muiTheme: ThemeManager.getMuiTheme(MyRawTheme),
         };
     },
 
@@ -49,9 +53,12 @@ export default React.createClass({
     },
     updateButton: function () {
         var style = {
-            margin: 12
+            margin: 12,
+            display:'block',
+            float:'right'
+
         };
-        return <div><RaisedButton label="Update" secondary={true} style={style} onClick={() => this.updateURL()}/></div>;
+        return <div><RaisedButton label="Update" primary={true} style={style} onClick={() => this.updateURL()}/></div>;
     },
 
     updateURL: function(){
@@ -95,99 +102,195 @@ export default React.createClass({
     checkBoxes: function(){
         var styles = {
             block: {
-                maxWidth: 250,
+                maxWidth: 256,
             },
             checkbox: {
                 marginBottom: 16,
             }
         };
-        return (<div style={styles.block}>
-                <h3>Favorite views</h3>
-                <Checkbox
-                    label="Map"
-                    checked={this.isChecked("Map views")}
-                    onCheck={()=>this.onChangeCheckbox("Map views")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Chart"
-                    defaultChecked={this.isChecked("Chart views")}
-                    onCheck={()=>this.onChangeCheckbox("Chart views")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Report table"
-                    defaultChecked={this.isChecked("Report table views")}
-                    onCheck={()=>this.onChangeCheckbox("Report table views")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Event report"
-                    defaultChecked={this.isChecked("Event report views")}
-                    onCheck={()=>this.onChangeCheckbox("Event report views")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Event chart"
-                    defaultChecked={this.isChecked("Event chart views")}
-                    onCheck={()=>this.onChangeCheckbox("Event chart views")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Dashboard"
-                    defaultChecked={this.isChecked("Dashboard views")}
-                    onCheck={()=>this.onChangeCheckbox("Dashboard views")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Indicator"
-                    defaultChecked={this.isChecked("Indicators views")}
-                    onCheck={()=>this.onChangeCheckbox("Indicators views")}
-                    style={styles.checkbox}
-                />
-                <h3>Saved</h3>
-                <Checkbox
-                    label="Maps"
-                    defaultChecked={this.isChecked("Saved maps")}
-                    onCheck={()=>this.onChangeCheckbox("Saved maps")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Charts"
-                    defaultChecked={this.isChecked("Saved charts")}
-                    onCheck={()=>this.onChangeCheckbox("Saved charts")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Report tables"
-                    defaultChecked={this.isChecked("Saved report tables")}
-                    onCheck={()=>this.onChangeCheckbox("Saved report tables")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Event reports"
-                    defaultChecked={this.isChecked("Saved event report")}
-                    onCheck={()=>this.onChangeCheckbox("Saved event report")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Event charts"
-                    defaultChecked={this.isChecked("Saved event charts")}
-                    onCheck={()=>this.onChangeCheckbox("Saved event charts")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Dashboards"
-                    defaultChecked={this.isChecked("Saved dashboards")}
-                    onCheck={()=>this.onChangeCheckbox("Saved dashboards")}
-                    style={styles.checkbox}
-                />
-                <Checkbox
-                    label="Indicators"
-                    defaultChecked={this.isChecked("Saved indicators")}
-                    onCheck={()=>this.onChangeCheckbox("Saved indicators")}
-                    style={styles.checkbox}
-                />
+        return (
+            <div style={styles.block}>
+                <table border="1" >
+                    <tr>
+                        <td><h4>Favorite views</h4>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Map"
+                                checked={this.isChecked("Map views")}
+                                onCheck={()=>this.onChangeCheckbox("Map views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Chart"
+                                defaultChecked={this.isChecked("Chart views")}
+                                onCheck={()=>this.onChangeCheckbox("Chart views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Indicator"
+                                defaultChecked={this.isChecked("Indicators views")}
+                                onCheck={()=>this.onChangeCheckbox("Indicators views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Dashboard"
+                                defaultChecked={this.isChecked("Dashboard views")}
+                                onCheck={()=>this.onChangeCheckbox("Dashboard views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Event chart"
+                                defaultChecked={this.isChecked("Event chart views")}
+                                onCheck={()=>this.onChangeCheckbox("Event chart views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Report table"
+                                defaultChecked={this.isChecked("Report table views")}
+                                onCheck={()=>this.onChangeCheckbox("Report table views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Event report"
+                                defaultChecked={this.isChecked("Event report views")}
+                                onCheck={()=>this.onChangeCheckbox("Event report views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><h4>Saved</h4></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Map"
+                                defaultChecked={this.isChecked("Saved maps")}
+                                onCheck={()=>this.onChangeCheckbox("Saved maps")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Chart"
+                                defaultChecked={this.isChecked("Saved charts")}
+                                onCheck={()=>this.onChangeCheckbox("Saved charts")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Indicator"
+                                defaultChecked={this.isChecked("Saved indicators")}
+                                onCheck={()=>this.onChangeCheckbox("Saved indicators")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Dashboard"
+                                defaultChecked={this.isChecked("Saved dashboards")}
+                                onCheck={()=>this.onChangeCheckbox("Saved dashboards")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Event chart"
+                                defaultChecked={this.isChecked("Saved event charts")}
+                                onCheck={()=>this.onChangeCheckbox("Saved event charts")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Report table"
+                                defaultChecked={this.isChecked("Saved report tables")}
+                                onCheck={()=>this.onChangeCheckbox("Saved report tables")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> <Checkbox
+                            label="Event report"
+                            defaultChecked={this.isChecked("Saved event report")}
+                            onCheck={()=>this.onChangeCheckbox("Saved event report")}
+                            style={styles.checkbox}
+                        /></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><h4>other</h4></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Active users"
+                                defaultChecked={this.isChecked("Active users")}
+                                onCheck={()=>this.onChangeCheckbox("Active users")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Average views"
+                                defaultChecked={this.isChecked("Average views")}
+                                onCheck={()=>this.onChangeCheckbox("Average views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <Checkbox
+                                label="Total views"
+                                defaultChecked={this.isChecked("Total views")}
+                                onCheck={()=>this.onChangeCheckbox("Total views")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                        <td>
+                            <Checkbox
+                                label="Total users"
+                                defaultChecked={this.isChecked("Total users")}
+                                onCheck={()=>this.onChangeCheckbox("Total users")}
+                                style={styles.checkbox}
+                            />
+                        </td>
+                    </tr>
+                </table>
             </div>
 
         );
@@ -198,21 +301,21 @@ export default React.createClass({
             sidebar: {
                 backgroundColor: '#f3f3f3',
                 overflowY: 'auto',
-                width: 256
-            }
+                width: 256,
+            },
+            inner:{padding: 5}
         };
         return (
             <div className="app-wrapper">
                 <HeaderBar />
                 {React.createElement(
-                'div',
-                {style: style.sidebar, className: 'left-bar'},
-                this.updateButton(),
-                this.DatePicker(),
-                this.dropdownMenu(),
-                this.checkBoxes()
-
-                )}
+                    'div',
+                    {style: style.sidebar, className: 'left-bar'},
+                        this.DatePicker(),
+                        this.updateButton(),
+                        this.dropdownMenu(),
+                        this.checkBoxes()
+                    )}
                 <div className="main-content">
                     <Client source={this.state.url} variables={this.state.showVariables}/>
                 </div>
---
+++ src/colortheme.js
@@ -0,0 +1,28 @@
+/**
+ * Created by JulieHillRoa on 31.03.2016.
+ */
+
+import Colors from 'material-ui/lib/styles/colors';
+import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
+import Spacing from 'material-ui/lib/styles/spacing';
+import zIndex from 'material-ui/lib/styles/zIndex';
+
+export default {
+    spacing: Spacing,
+    zIndex: zIndex,
+    fontFamily: 'Roboto, sans-serif',
+    palette: {
+        primary1Color: "#276696",
+        primary2Color: "#276696",
+        primary3Color: Colors.lightBlack,
+        accent1Color: "#276696",
+        accent2Color: Colors.grey100,
+        accent3Color: Colors.grey500,
+        textColor: Colors.darkBlack,
+        alternateTextColor: Colors.white,
+        canvasColor: Colors.white,
+        borderColor: Colors.grey300,
+        disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3),
+        pickerHeaderColor: "#276696",
+    }
+};
