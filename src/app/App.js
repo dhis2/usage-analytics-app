a3591d83e63c8279115080c9ecfdf636c738d671
Julie Hill Roa <julie.hill.roa@gmail.com>
2016-04-05 15:41:14 +0200

    changes sidebar: new drop down and design

--- src/app/App.js
+++ src/app/App.js
@@ -34,19 +34,25 @@ export default React.createClass({
     },
 
     getInitialState: function () {
+        let minDate = new Date();
+        let maxDate = new Date();
+        minDate = (minDate.getMonth() + 1)+"/"+ minDate.getDate()+"/"+(minDate.getFullYear() - 1);
+        maxDate = (maxDate.getMonth() + 1)+"/"+ maxDate.getDate()+"/"+maxDate.getFullYear();
+
         //all variables that can be retrieved from server. Extend if server is changed
-        var allVarables = ["Active users","Map views","Chart views",
+       /* var allVarables = ["Active users","Map views","Chart views",
             "Report table views","Event report views","Event chart views",
             "Dashboard views","Indicators views","Total views","Average views","Saved maps",
             "Saved charts","Saved report tables","Saved event report",
             "Saved event charts","Saved dashboards","Saved indicators",
-            "Total users"];
+            "Total users"];*/
         return this.state = {
-            value: 'YEAR',
-            startDate: '',
-            endDate: '',
+            interval: 'DAY',
+            startDate: minDate,
+            endDate: maxDate,
             url: '',
-            showVariables: allVarables
+            category:'FavoriteViews'
+            //showVariables: allVarables
         };
     },
     updateButton: function () {
@@ -60,17 +66,24 @@ export default React.createClass({
     },
 
     updateURL: function(){
-        let tempUrl = "http://localhost:8080/api/dataStatistics?startDate="+this.state.startDate+"&endDate="+this.state.endDate+"&interval=" + this.state.value;
+        let tempUrl = "http://localhost:8080/api/dataStatistics?startDate="+this.state.startDate+"&endDate="+this.state.endDate+"&interval=" + this.state.interval;
         this.setState({url:tempUrl});
     },
 
     DatePicker: function () {
         var style = {
-            marginLeft: 5
+            label:{
+                display:'block',
+                fontFamily: 'Roboto, sans-serif',
+
+            },
+            marginLeft: 10
         };
         return ( <div style={style}>
-            <DatePicker hintText="Start date" mode="landscape" onChange={(event, value) => this.setState({startDate: this.formatDate(value)})} />
-            <DatePicker hintText="End date" mode="landscape" onChange={(event, value) => this.setState({endDate: this.formatDate(value)})}/>
+            <label style={style.label} htmlFor="start"><b>Start date:</b></label>
+            <DatePicker id="start" defaultValue={this.state.startDate} mode="landscape" onChange={(event, value) => this.setState({startDate: this.formatDate(value)})} />
+            <label style={style.label} htmlFor="end"><b>End date:</b></label>
+            <DatePicker id="end" defaultValue={this.state.endDate} mode="landscape" onChange={(event, value) => this.setState({endDate: this.formatDate(value)})}/>
         </div>);
     },
 
@@ -79,226 +92,45 @@ export default React.createClass({
     },
 
 
-    dropdownMenu: function () {
-        return (
-            <DropDownMenu value={this.state.value} onChange={(event, index, value) => this.setState({value: value})}>
+    dropdownIntervalMenu: function () {
+        var style = {
+            label:{
+                display:'block',
+                fontFamily: 'Roboto, sans-serif',
+                marginLeft: 10
+            }
+
+        };
+        return (<div>
+            <label style={style.label} htmlFor="intervaldrop"><b>Interval:</b></label>
+            <DropDownMenu id="intervaldrop" value={this.state.interval} onChange={(event, index, value) => this.setState({interval: value})}>
                 <MenuItem value={'DAY'} primaryText="DAY"/>
                 <MenuItem value={'WEEK'} primaryText="WEEK"/>
                 <MenuItem value={'MONTH'} primaryText="MONTH"/>
                 <MenuItem value={'YEAR'} primaryText="YEAR"/>
-            </DropDownMenu>);
+            </DropDownMenu></div>);
 
     },
 
-    onChangeCheckbox: function(box){
-        if(this.state.showVariables.indexOf(box) > -1)
-            this.setState({showVariables: this.state.showVariables.filter((value,index) => value !== box ) });
-        else this.setState({showVariables: this.state.showVariables.concat(box)});
-    },
-
-    isChecked: function(box){
-        return this.state.showVariables.indexOf(box) > -1;
-    },
-
-    checkBoxes: function(){
-        var styles = {
-            checkbox: {
-                marginBottom: 6,
-            },
-            font:{
+    dropdownCategoryMenu: function () {
+        var style = {
+            label:{
+                display:'block',
                 fontFamily: 'Roboto, sans-serif',
-                fontWeight: 'normal'
+                marginLeft: 10
             }
+
         };
-        return (
-            <div style={styles.block}>
-                <table>
-                    <tbody>
-                    <tr>
-                        <td><h4 style={styles.font}>Favorite views</h4>
-                        </td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Map"
-                                checked={this.isChecked("Map views")}
-                                onCheck={()=>this.onChangeCheckbox("Map views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Chart"
-                                defaultChecked={this.isChecked("Chart views")}
-                                onCheck={()=>this.onChangeCheckbox("Chart views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
+        return (<div>
+            <label style={style.label} htmlFor="Categorydrop"><b>Category:</b></label>
+            <DropDownMenu id="Categorydrop" value={this.state.category} onChange={(event, index, value) => this.setState({category: value})}>
+                <MenuItem value={'FavoriteViews'} primaryText="Favorite views"/>
+                <MenuItem value={'Favoritesaved'} primaryText="Favorite saved"/>
+                <MenuItem value={'Users'} primaryText="Users"/>
+            </DropDownMenu></div>);
 
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Indicator"
-                                defaultChecked={this.isChecked("Indicators views")}
-                                onCheck={()=>this.onChangeCheckbox("Indicators views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Dashboard"
-                                defaultChecked={this.isChecked("Dashboard views")}
-                                onCheck={()=>this.onChangeCheckbox("Dashboard views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Event chart"
-                                defaultChecked={this.isChecked("Event chart views")}
-                                onCheck={()=>this.onChangeCheckbox("Event chart views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Report table"
-                                defaultChecked={this.isChecked("Report table views")}
-                                onCheck={()=>this.onChangeCheckbox("Report table views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Event report"
-                                defaultChecked={this.isChecked("Event report views")}
-                                onCheck={()=>this.onChangeCheckbox("Event report views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><h4 style={styles.font}>Saved</h4></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Map"
-                                defaultChecked={this.isChecked("Saved maps")}
-                                onCheck={()=>this.onChangeCheckbox("Saved maps")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Chart"
-                                defaultChecked={this.isChecked("Saved charts")}
-                                onCheck={()=>this.onChangeCheckbox("Saved charts")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Indicator"
-                                defaultChecked={this.isChecked("Saved indicators")}
-                                onCheck={()=>this.onChangeCheckbox("Saved indicators")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Dashboard"
-                                defaultChecked={this.isChecked("Saved dashboards")}
-                                onCheck={()=>this.onChangeCheckbox("Saved dashboards")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Event chart"
-                                defaultChecked={this.isChecked("Saved event charts")}
-                                onCheck={()=>this.onChangeCheckbox("Saved event charts")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Report table"
-                                defaultChecked={this.isChecked("Saved report tables")}
-                                onCheck={()=>this.onChangeCheckbox("Saved report tables")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td> <Checkbox
-                            label="Event report"
-                            defaultChecked={this.isChecked("Saved event report")}
-                            onCheck={()=>this.onChangeCheckbox("Saved event report")}
-                            style={styles.checkbox}
-                        /></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td><h4 style={styles.font}>Other</h4></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Active users"
-                                defaultChecked={this.isChecked("Active users")}
-                                onCheck={()=>this.onChangeCheckbox("Active users")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Average views"
-                                defaultChecked={this.isChecked("Average views")}
-                                onCheck={()=>this.onChangeCheckbox("Average views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <Checkbox
-                                label="Total views"
-                                defaultChecked={this.isChecked("Total views")}
-                                onCheck={()=>this.onChangeCheckbox("Total views")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                        <td>
-                            <Checkbox
-                                label="Total users"
-                                defaultChecked={this.isChecked("Total users")}
-                                onCheck={()=>this.onChangeCheckbox("Total users")}
-                                style={styles.checkbox}
-                            />
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-
-        );
     },
+
     render()
     {
         var style = {
@@ -306,8 +138,7 @@ export default React.createClass({
                 backgroundColor: '#f3f3f3',
                 overflowY: 'auto',
                 width: 256,
-            },
-            inner:{padding: 5}
+            }
         };
         return (
             <div className="app-wrapper">
@@ -316,12 +147,12 @@ export default React.createClass({
                     'div',
                     {style: style.sidebar, className: 'left-bar'},
                         this.DatePicker(),
-                        this.updateButton(),
-                        this.dropdownMenu(),
-                        this.checkBoxes()
+                        this.dropdownIntervalMenu(),
+                        this.dropdownCategoryMenu(),
+                        this.updateButton()
                     )}
                 <div className="main-content">
-                    <Client source={this.state.url} variables={this.state.showVariables}/>
+                    <Client source={this.state.url} variables={this.state.category}/>
                 </div>
             </div>
         );
