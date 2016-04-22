f7cc22facfab36bde9f70a4ce1be974d195c842d
Julie Hill Roa <julie.hill.roa@gmail.com>
2016-04-22 19:52:00 +0200

    changed table to consider average per favorite

--- src/app/Table.js
+++ src/app/Table.js
@@ -6,104 +6,15 @@ import TableHeader from 'material-ui/lib/table/table-header';
 import TableRowColumn from 'material-ui/lib/table/table-row-column';
 import TableBody from 'material-ui/lib/table/table-body';
 
+const style = {
+    header: {
+        fontSize: 15,
+        color: '#000000',
+        backgroundColor: '#f3f3f3'
+    }
+};
+
 export default React.createClass({
-
-    favoreiteViewsOnly: function (data) {
-        let arr = [];
-        data.filter((val) => {
-            return (arr.push({
-                Date: this.getDate(val.year, val.month, val.week, val.day),
-                Mapview: val.mapViews,
-                Chartview: val.chartViews,
-                ReportTableview: val.reportTableViews,
-                EventReportview: val.eventReportViews,
-                EventChartview: val.eventChartViews,
-                Dashboardview: val.dashboardViews,
-                Totalview: val.totalViews,
-                Averageview: val.averageViews
-            }))
-        });
-        return arr;
-    },
-    favoreiteSavedOnly: function (data) {
-        let arr = [];
-        data.filter((val) => {
-            return (arr.push({
-                Date: this.getDate(val.year, val.month, val.week, val.day),
-                Mapssaved: val.savedMaps,
-                Chartssaved: val.savedCharts,
-                ReportTablessaved: val.savedReportTables,
-                EventReportssaved: val.savedEventReports,
-                EventChartssaved: val.savedEventCharts,
-                Dashboardssaved: val.savedDashboards,
-                Indicatorssaved: val.savedIndicators
-            }))
-        });
-        return arr;
-    },
-    usersOnly: function (data) {
-        let arr = [];
-        data.filter((val) => {
-            return (arr.push({
-                Date: this.getDate(val.year, val.month, val.week, val.day),
-                Activeusers: val.activeUsers,
-                Totalusers: val.users
-            }))
-        });
-        return arr;
-    },
-
-    getDate: function (year, month, week, day) {
-        let date = year;
-        if (month != null) {
-            let monthText = '';
-            if (month == 1) {
-                monthText = ' jan';
-            }
-            else if (month == 2) {
-                monthText = ' feb';
-            }
-            else if (month == 3) {
-                monthText = ' mar';
-            }
-            else if (month == 4) {
-                monthText = ' apr';
-            }
-            else if (month == 5) {
-                monthText = ' may';
-            }
-            else if (month == 6) {
-                monthText = ' jun';
-            }
-            else if (month == 7) {
-                monthText = ' jul';
-            }
-            else if (month == 8) {
-                monthText = ' aug';
-            }
-            else if (month == 9) {
-                monthText = ' sep';
-            }
-            else if (month == 10) {
-                monthText = ' oct';
-            }
-            else if (month == 11) {
-                monthText = ' nov';
-            }
-            else if (month == 12) {
-                monthText = ' dec';
-            }
-
-            date = year + monthText;
-            if (day != null) {
-                date += ' ' + day;
-            }
-        }
-        if (week != null) {
-            date += ' / ' + week;
-        }
-        return date;
-    },
 
     createRow: function (row) {
         let rows = [];
@@ -116,48 +27,43 @@ export default React.createClass({
 
     },
     createHeaderRow: function (headers, category) {
-
-        const style = {
-            header: {
-                fontSize: 15,
-                color: '#000000',
-                backgroundColor: '#f3f3f3'
-            }
-        };
         let rows = [];
         let subOne = '';
         let subTwo = '';
+        let subTree = '';
         for (var key in headers) {
             if (key != 'Date') {
-                let index = key.indexOf(category);
-                subOne = key.slice(0, index).trim();
-                if (category == 'users') {
+                if (category == "Favorite Views") {
+                    let index = key.indexOf('Views');
+                    if(key.indexOf('Average') > -1){
+                        subOne = key.slice(0, 7).trim();
+                        subTwo = key.slice(7, index).trim();
+                    }
+                    else{
+                        subOne = key.slice(0, index).trim();
+                    }
+
+                } else if (this.props.category == "Favorite saved") {
+                    subOne = key.slice(5, key.length).trim();
+                } else if (this.props.category == "Users") {
+                    let index = key.indexOf('Users');
+                    subOne = key.slice(0, index).trim();
                     subTwo = key.slice(index, key.length).trim();
+
                 }
-            } else {
+            }else {
                 subOne = key;
             }
-            rows.push(<TableHeaderColumn style={style.header}><b>{subOne} {subTwo}</b></TableHeaderColumn>);
+            rows.push(<TableHeaderColumn style={style.header}><b>{subOne} {subTwo} {subTree}</b></TableHeaderColumn>);
         }
         return ( <TableRow>{rows}</TableRow>);
     },
 
     createTable: function () {
-        let category = '';
-        let input = null;
-        if (this.props.category == "Favorite Views") {
-            input = this.favoreiteViewsOnly(this.props.data);
-            category = 'view';
-        } else if (this.props.category == "Favorite saved") {
-            input = this.favoreiteSavedOnly(this.props.data);
-            category = 'saved';
-        } else if (this.props.category == "Users") {
-            input = this.usersOnly(this.props.data);
-            category = 'users';
-        }
+        let input = this.props.data;
         return ( <Table fixedHeader={true}>
                 <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-                    {this.createHeaderRow(input[0], category)}
+                    {this.createHeaderRow(input[0], this.props.category)}
                 </TableHeader>
                 <TableBody displayRowCheckbox={false}>
                     {input.map((value, index, collection) => this.createRow(value))}
