1d79fba6e66435da664200c2514a67343fc17138
Hendrik de Graaf <hendrik@dhis2.org>
2018-12-11 11:53:37 +0100

    chore: test setup - Adds @babel/preset-env to babel presets so ui files are transpiled for tests - Moves jest config to a JS file so DHIS_CONF can be added to globals in the test context - git ignore generated manifest file

---
+++ jest.config.js
@@ -0,0 +1,38 @@
+const dhisConfig = require('./config/dhisConfig')
+
+module.exports = {
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+    resolver: 'jest-pnp-resolver',
+    setupFiles: ['react-app-polyfill/jsdom'],
+    testMatch: [
+        '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+        '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
+    ],
+    testEnvironment: 'jsdom',
+    testURL: 'http://localhost',
+    transform: {
+        '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
+        '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+        '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)':
+            '<rootDir>/config/jest/fileTransform.js',
+    },
+    transformIgnorePatterns: [
+        '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+        '^.+\\.module\\.(css|sass|scss)$',
+    ],
+    moduleFileExtensions: [
+        'web.js',
+        'js',
+        'web.ts',
+        'ts',
+        'web.tsx',
+        'tsx',
+        'json',
+        'web.jsx',
+        'jsx',
+        'node',
+    ],
+    globals: {
+        DHIS_CONFIG: dhisConfig,
+    },
+}
