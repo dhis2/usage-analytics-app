6f80eaf6bf916d03f1d5cdf079ba8dd1eb15dcc2
ismay <ismay@ismaywolff.nl>
2019-05-14 11:16:38 +0200

    chore: update dependencies (#42)
    * chore: update dependencies
    
    * chore: add custom serializer
    
    * chore: mock react-redux instead of custom serializer

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import App from './App'
 
+beforeEach(() => {
+    jest.resetModules()
+})
+
 describe('<App/>', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div')
+
         ReactDOM.render(<App />, div)
         ReactDOM.unmountComponentAtNode(div)
     })
-    it('Matches the snapshot', () => {
+
+    it('renders the expected tree', () => {
+        jest.doMock('react-redux', () => ({
+            connect: jest.fn(() => component => `Connected${component.name}`),
+            Provider: () => <div />,
+        }))
+        const App = require('./App').default
         const tree = shallow(<App />)
+
         expect(tree).toMatchSnapshot()
     })
 })
--- src/components/UsageAnalytics.test.js
+++ src/components/UsageAnalytics.test.js
@@ -3,6 +3,10 @@ import { shallow } from 'enzyme'
 import { UsageAnalytics } from './UsageAnalytics'
 import { LOADING, READY, ERROR } from '../constants/statuses'
 
+jest.mock('react-redux', () => ({
+    connect: jest.fn(() => component => `Connected${component.name}`),
+}))
+
 const defaultProps = {
     initApp: jest.fn(),
     appStatus: READY,
@@ -11,17 +15,20 @@ const defaultProps = {
 describe('<UsageAnalytics/>', () => {
     it('Matches the snapshot', () => {
         const tree = shallow(<UsageAnalytics {...defaultProps} />)
+
         expect(tree).toMatchSnapshot()
     })
     it('Renders a <CircularProgress/> when appStatus equals LOADING', () => {
         const props = { ...defaultProps, appStatus: LOADING }
         const wrapper = shallow(<UsageAnalytics {...props} />)
+
         expect(wrapper.find('CircularProgress').length).toEqual(1)
     })
 
     it('Renders an <Error/> when appStatus equals ERROR', () => {
         const props = { ...defaultProps, appStatus: ERROR }
         const wrapper = shallow(<UsageAnalytics {...props} />)
+
         expect(wrapper.find('Error').length).toEqual(1)
     })
 })
