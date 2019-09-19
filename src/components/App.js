b8350a4fc5da9278689e981bfbaeed4299019636
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-09-19 20:02:01 +0200

    fix: fix invalid reducer, add index.css

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import store from '../store'
 import UsageAnalytics from './UsageAnalytics'
+import '../index.css'
 
 function App() {
     return (
