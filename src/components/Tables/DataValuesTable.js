65f3221d29da3885013f646454f3ea93405af07c
ismay <ismay@ismaywolff.nl>
2020-11-05 16:35:21 +0100

    refactor(date-helper): use simplified date helper

--- src/components/Tables/DataValuesTable.js
+++ src/components/Tables/DataValuesTable.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import PropTypes from '@dhis2/prop-types'
 import i18n from '@dhis2/d2-i18n'
 import {
@@ -10,39 +10,33 @@ import {
     TableCellHead,
     TableBody,
 } from '@dhis2/ui'
-import { getDisplayDateForInterval } from '../../utils/date.js'
-import { LocaleContext } from '../Locale'
+import { formatIntervalDate } from '../../utils/date.js'
 
-const DataValuesTable = ({ data, interval }) => {
-    const locale = useContext(LocaleContext)
+const DataValuesTable = ({ data, interval }) => (
+    <Table>
+        <TableHead>
+            <TableRowHead>
+                <TableCellHead>{i18n.t('Date')}</TableCellHead>
+                <TableCellHead>{i18n.t('Data Values')}</TableCellHead>
+            </TableRowHead>
+        </TableHead>
+        <TableBody>
+            {data.map(({ year, month, week, day, savedDataValues }) => {
+                const formatted = formatIntervalDate(
+                    { year, month, week, day },
+                    interval
+                )
 
-    return (
-        <Table>
-            <TableHead>
-                <TableRowHead>
-                    <TableCellHead>{i18n.t('Date')}</TableCellHead>
-                    <TableCellHead>{i18n.t('Data Values')}</TableCellHead>
-                </TableRowHead>
-            </TableHead>
-            <TableBody>
-                {data.map(({ year, month, week, day, savedDataValues }) => {
-                    const timestamp = getDisplayDateForInterval(
-                        { year, month, week, day },
-                        interval,
-                        locale
-                    )
-
-                    return (
-                        <TableRow key={timestamp}>
-                            <TableCell>{timestamp}</TableCell>
-                            <TableCell>{savedDataValues}</TableCell>
-                        </TableRow>
-                    )
-                })}
-            </TableBody>
-        </Table>
-    )
-}
+                return (
+                    <TableRow key={formatted}>
+                        <TableCell>{formatted}</TableCell>
+                        <TableCell>{savedDataValues}</TableCell>
+                    </TableRow>
+                )
+            })}
+        </TableBody>
+    </Table>
+)
 
 DataValuesTable.propTypes = {
     data: PropTypes.array.isRequired,
