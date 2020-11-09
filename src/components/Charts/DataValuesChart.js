ed8e8dec8f31296fb7c998c59176db3f836aba5c
ismay <ismay@ismaywolff.nl>
2020-11-09 16:41:15 +0100

    fix(chart): set options globally per line chart

--- src/components/Charts/DataValuesChart.js
+++ src/components/Charts/DataValuesChart.js
@@ -1,9 +1,43 @@
 import React from 'react'
 import PropTypes from '@dhis2/prop-types'
+import i18n from '@dhis2/d2-i18n'
 import parseChartData from './parseChartData.js'
 import { Line } from 'react-chartjs-2'
 import ChartWrapper from './ChartWrapper.js'
 import { getTitles } from './selectors.js'
+
+/**
+ * For documentation on the options see:
+ * https://github.com/reactchartjs/react-chartjs-2
+ * https://www.chartjs.org/docs/latest/
+ */
+
+const options = {
+    animation: {
+        duration: 180,
+    },
+    legend: {
+        position: 'bottom',
+    },
+    maintainAspectRatio: false,
+    elements: {
+        line: {
+            tension: 0,
+            fill: false,
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+        },
+    },
+    scales: {
+        yAxes: [
+            {
+                scaleLabel: {
+                    display: true,
+                    labelString: i18n.t('Values'),
+                },
+            },
+        ],
+    },
+}
 
 const DataValuesChart = ({
     data,
@@ -20,7 +54,7 @@ const DataValuesChart = ({
         interval,
         data,
     })
-    const { options, data: lineData } = parsed
+    const lineData = parsed
 
     return (
         <ChartWrapper title={title} subtitle={subtitle}>
