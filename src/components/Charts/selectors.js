fc66682e277820d0ec5facfca9263c10ef900133
ismay <ismay@ismaywolff.nl>
2020-11-17 14:17:20 +0100

    chore(graph-colors): use constants for graph colors

--- src/components/Charts/selectors.js
+++ src/components/Charts/selectors.js
@@ -2,6 +2,19 @@ import i18n from '@dhis2/d2-i18n'
 import CATEGORIES from '../../constants/categories'
 import { getIntervalDate } from '../../selectors/date.js'
 
+/**
+ * The colors for the linegraphs. We're using individual constants
+ * so eslint will warn us if we use a constant that doesn't exist.
+ */
+
+const COLOR_1 = '#7cb5ec'
+const COLOR_2 = '#cc6600'
+const COLOR_3 = '#cccc00'
+const COLOR_4 = '#66cc00'
+const COLOR_5 = '#ff0066'
+const COLOR_6 = '#000000'
+const COLOR_7 = '#00cccc'
+
 export const getTitles = selected => {
     const selectedCategory = CATEGORIES.find(
         category => category.value === selected
@@ -22,7 +35,7 @@ export const getDataValuesDatasets = data => [
     {
         label: i18n.t('Data Values'),
         data: data.map(d => d.savedDataValues),
-        borderColor: '#7cb5ec',
+        borderColor: COLOR_1,
     },
 ]
 
@@ -30,12 +43,12 @@ export const getUsersDatasets = data => [
     {
         label: i18n.t('Active Users'),
         data: data.map(d => d.activeUsers),
-        borderColor: '#7cb5ec',
+        borderColor: COLOR_1,
     },
     {
         label: i18n.t('Total Users'),
         data: data.map(d => d.users),
-        borderColor: '#cc6600',
+        borderColor: COLOR_2,
     },
 ]
 
@@ -43,37 +56,37 @@ export const getFavoritesSavedDatasets = data => [
     {
         label: i18n.t('Maps'),
         data: data.map(d => d.savedMaps),
-        borderColor: '#7cb5ec',
+        borderColor: COLOR_1,
     },
     {
         label: i18n.t('Charts'),
         data: data.map(d => d.savedCharts),
-        borderColor: '#cc6600',
+        borderColor: COLOR_2,
     },
     {
         label: i18n.t('Pivot Tables'),
         data: data.map(d => d.savedPivotTables),
-        borderColor: '#cccc00',
+        borderColor: COLOR_3,
     },
     {
         label: i18n.t('Event Reports'),
         data: data.map(d => d.savedEventReports),
-        borderColor: '#66cc00',
+        borderColor: COLOR_4,
     },
     {
         label: i18n.t('Event Charts'),
         data: data.map(d => d.savedEventCharts),
-        borderColor: '#ff0066',
+        borderColor: COLOR_5,
     },
     {
         label: i18n.t('Dashboards'),
         data: data.map(d => d.savedDashboards),
-        borderColor: '#000000',
+        borderColor: COLOR_6,
     },
     {
         label: i18n.t('Indicators'),
         data: data.map(d => d.savedIndicators),
-        borderColor: '#00cccc',
+        borderColor: COLOR_7,
     },
 ]
 
@@ -81,37 +94,37 @@ export const getSumAllFavoriteViewsDatasets = data => [
     {
         label: i18n.t('Map'),
         data: data.map(d => d.mapViews),
-        borderColor: '#7cb5ec',
+        borderColor: COLOR_1,
     },
     {
         label: i18n.t('Chart'),
         data: data.map(d => d.chartViews),
-        borderColor: '#cc6600',
+        borderColor: COLOR_2,
     },
     {
         label: i18n.t('Pivot Table'),
         data: data.map(d => d.pivotTableViews),
-        borderColor: '#cccc00',
+        borderColor: COLOR_3,
     },
     {
         label: i18n.t('Event Report'),
         data: data.map(d => d.eventReportViews),
-        borderColor: '#66cc00',
+        borderColor: COLOR_4,
     },
     {
         label: i18n.t('Event Chart'),
         data: data.map(d => d.eventChartViews),
-        borderColor: '#ff0066',
+        borderColor: COLOR_5,
     },
     {
         label: i18n.t('Dashboard'),
         data: data.map(d => d.dashboardViews),
-        borderColor: '#000000',
+        borderColor: COLOR_6,
     },
     {
         label: i18n.t('Data Set Report'),
         data: data.map(d => d.dataSetReportViews),
-        borderColor: '#00cccc',
+        borderColor: COLOR_7,
     },
 ]
 
@@ -119,7 +132,7 @@ export const getSumTotalFavoriteViewsDatasets = data => [
     {
         label: i18n.t('Total'),
         data: data.map(d => d.totalViews),
-        borderColor: '#7cb5ec',
+        borderColor: COLOR_1,
     },
 ]
 
@@ -127,32 +140,32 @@ export const getAverageAllFavoriteViewsDatasets = data => [
     {
         label: i18n.t('Average Map'),
         data: data.map(d => d.averageMapViews),
-        borderColor: '#7cb5ec',
+        borderColor: COLOR_1,
     },
     {
         label: i18n.t('Average Chart'),
         data: data.map(d => d.averageChartViews),
-        borderColor: '#cc6600',
+        borderColor: COLOR_2,
     },
     {
         label: i18n.t('Average Pivot Table'),
         data: data.map(d => d.averagePivotTableViews),
-        borderColor: '#cccc00',
+        borderColor: COLOR_3,
     },
     {
         label: i18n.t('Average Event Report'),
         data: data.map(d => d.averageEventReportViews),
-        borderColor: '#66cc00',
+        borderColor: COLOR_4,
     },
     {
         label: i18n.t('Average Event Chart'),
         data: data.map(d => d.averageEventChartViews),
-        borderColor: '#ff0066',
+        borderColor: COLOR_5,
     },
     {
         label: i18n.t('Average Dashboard'),
         data: data.map(d => d.averageDashboardViews),
-        borderColor: '#000000',
+        borderColor: COLOR_6,
     },
 ]
 
@@ -160,6 +173,6 @@ export const getAverageTotalFavoriteViewsDatasets = data => [
     {
         label: i18n.t('Average'),
         data: data.map(d => d.averageViews),
-        borderColor: '#7cb5ec',
+        borderColor: COLOR_1,
     },
 ]
