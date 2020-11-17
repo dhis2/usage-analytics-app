db1df97aa7064d2d07c16d88a4c0b284781430c8
ismay <ismay@ismaywolff.nl>
2020-11-17 14:09:21 +0100

    chore(app): improve date helper comment

--- src/components/App/createDefaultDates.js
+++ src/components/App/createDefaultDates.js
@@ -4,8 +4,9 @@ import moment from 'moment'
  * This creates the default start and end dates for the app state. The format
  * is YYYY-MM-DD because that's what a date input expects, and that's what these
  * dates will be used for.
- * The default is to have the end date be now, and the start date 4 months before
- * that.
+ *
+ * The default is to have the end date at the current time, and the start date
+ * 4 months before that.
  */
 
 const createDefaultDates = () => {
