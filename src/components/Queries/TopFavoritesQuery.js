b710b878d9a1946af1bda91c05966d3e34f73ff0
ismay <ismay@ismaywolff.nl>
2020-11-17 17:27:10 +0100

    fix(translations): remove underscore from interpolation

--- src/components/Queries/TopFavoritesQuery.js
+++ src/components/Queries/TopFavoritesQuery.js
@@ -54,13 +54,10 @@ const TopFavoritesQuery = ({
 
     if (error) {
         const title = i18n.t('Error whilst fetching data')
-        const message = i18n.t(
-            'The error message was: "{{ ERROR_MESSAGE }}".',
-            {
-                ERROR_MESSAGE: error.message,
-                nsSeparator: '>',
-            }
-        )
+        const message = i18n.t('The error message was: "{{ MESSAGE }}".', {
+            MESSAGE: error.message,
+            nsSeparator: '>',
+        })
         const fallback = i18n.t(
             'There was no error message included with the error.'
         )
