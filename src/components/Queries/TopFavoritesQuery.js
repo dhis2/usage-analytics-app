bf6206977a34648f74907b81ee8ccd632b99df38
ismay <ismay@ismaywolff.nl>
2020-11-17 16:49:01 +0100

    fix(error-handling): show noticebox with error instead of throwing

--- src/components/Queries/TopFavoritesQuery.js
+++ src/components/Queries/TopFavoritesQuery.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react'
+import i18n from '@dhis2/d2-i18n'
 import PropTypes from '@dhis2/prop-types'
 import { useDataQuery } from '@dhis2/app-runtime'
-import { CircularLoader, ComponentCover, CenteredContent } from '@dhis2/ui'
+import {
+    CircularLoader,
+    ComponentCover,
+    CenteredContent,
+    NoticeBox,
+} from '@dhis2/ui'
 
 const query = {
     favorites: {
@@ -47,7 +53,23 @@ const TopFavoritesQuery = ({
     }
 
     if (error) {
-        throw error
+        const title = i18n.t('Error whilst fetching data')
+        const message = i18n.t(
+            'The error message was: "{{ ERROR_MESSAGE }}".',
+            {
+                ERROR_MESSAGE: error.message,
+                nsSeparator: '>',
+            }
+        )
+        const fallback = i18n.t(
+            'There was no error message included with the error.'
+        )
+
+        return (
+            <NoticeBox error title={title}>
+                {error.message ? message : fallback}
+            </NoticeBox>
+        )
     }
 
     return children(data.favorites)
