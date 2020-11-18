1a447c13d1d306dea24d302b3c7a77563c049a8b
ismay <ismay@ismaywolff.nl>
2020-11-18 11:39:44 +0100

    test(date): add date helper tests

---
+++ src/selectors/date.test.js
@@ -0,0 +1,28 @@
+import { getIntervalDate } from './date.js'
+import { YEAR, MONTH, WEEK, DAY } from '../constants/intervals.js'
+
+describe('getIntervalDate', () => {
+    it('returns the expected string for the YEAR interval', () => {
+        const actual = getIntervalDate({ year: 2000 }, YEAR)
+
+        expect(actual).toBe('2000')
+    })
+
+    it('returns the expected string for the MONTH interval', () => {
+        const actual = getIntervalDate({ year: 2000, month: 1 }, MONTH)
+
+        expect(actual).toBe('Jan 2000')
+    })
+
+    it('returns the expected string for the WEEK interval', () => {
+        const actual = getIntervalDate({ year: 2000, week: 1 }, WEEK)
+
+        expect(actual).toBe('Week 1 / 2000')
+    })
+
+    it('returns the expected string for the DAY interval', () => {
+        const actual = getIntervalDate({ year: 2000, month: 2, day: 3 }, DAY)
+
+        expect(actual).toBe('Feb 3, 2000')
+    })
+})
