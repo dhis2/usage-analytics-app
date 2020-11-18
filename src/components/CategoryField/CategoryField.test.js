28dbcb1118489ba4de2e09569f6e5ffbb70ff900
ismay <ismay@ismaywolff.nl>
2020-11-18 16:28:06 +0100

    test(categoryfield): add categoryfield test

---
+++ src/components/CategoryField/CategoryField.test.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import categories, {
+    FAVORITE_VIEWS,
+    FAVORITES_SAVED,
+} from '../../constants/categories.js'
+import CategoryField from './CategoryField.js'
+
+describe('<CategoryField>', () => {
+    it('has the expected label', () => {
+        const props = {
+            category: FAVORITE_VIEWS,
+            setCategory: () => {},
+        }
+        const wrapper = mount(<CategoryField {...props} />)
+        const label = wrapper.find('label')
+
+        expect(label).toHaveLength(1)
+        expect(label.text()).toBe('Category')
+    })
+
+    it('shows the current selection', () => {
+        const props = {
+            category: FAVORITE_VIEWS,
+            setCategory: () => {},
+        }
+        const wrapper = mount(<CategoryField {...props} />)
+        const singleselect = wrapper.find({
+            'data-test': 'dhis2-uicore-singleselect',
+        })
+
+        expect(singleselect.text()).toBe('Favorite views')
+    })
+
+    it('shows the expected options', () => {
+        const props = {
+            category: FAVORITE_VIEWS,
+            setCategory: () => {},
+        }
+        const wrapper = mount(<CategoryField {...props} />)
+        const input = wrapper.find({ 'data-test': 'dhis2-uicore-select-input' })
+
+        // Open menu
+        input.simulate('click')
+
+        const options = wrapper.find({
+            'data-test': 'dhis2-uicore-singleselectoption',
+        })
+
+        expect.assertions(5)
+
+        options.forEach((option, i) => {
+            expect(option.text()).toBe(categories[i].label)
+        })
+
+        // Umounting manually here prevents an act warning caused by Popper
+        wrapper.unmount()
+    })
+
+    it('calls setCategory with the expected value on selection change', () => {
+        const spy = jest.fn()
+        const props = {
+            category: FAVORITE_VIEWS,
+            setCategory: spy,
+        }
+        const wrapper = mount(<CategoryField {...props} />)
+        const input = wrapper.find({ 'data-test': 'dhis2-uicore-select-input' })
+
+        // Open menu
+        input.simulate('click')
+
+        // Find an option that is not currently selected
+        const target = wrapper.find({
+            value: FAVORITES_SAVED,
+        })
+
+        // Select the option
+        target.simulate('click')
+
+        expect(spy).toHaveBeenCalledWith(FAVORITES_SAVED)
+
+        // Umounting manually here prevents an act warning caused by Popper
+        wrapper.unmount()
+    })
+})
