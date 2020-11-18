e1cda7f2cd9d9b80714de833557e81e90e9dc57d
ismay <ismay@ismaywolff.nl>
2020-11-18 11:43:01 +0100

    test(table): add missing table component test

---
+++ src/components/Tables/TableWrapper.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import TableWrapper from './TableWrapper.js'
+
+describe('<TableWrapper>', () => {
+    it('renders without errors', () => {
+        shallow(<TableWrapper />)
+    })
+})
